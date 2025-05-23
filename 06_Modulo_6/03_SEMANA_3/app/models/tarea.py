7c77625e464a5ae2d7dc0b83db5e819b39a00015
luisguillermomolero <luisguillermomolero@gmail.com>
2025-05-22 21:36:37 -0500

    commit

---
+++ 06_Modulo_6/03_SEMANA_3/app/models/tarea.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from database.base import Base
+
+class Tarea(Base):
+    __tablename__  = "tareas"
+    
+    id = Column(Integer,  primary_key=True, index=True)
+    titulo = Column(String, nullable=False)
+    descripcion = Column(String)
+    completado = Column(Boolean, default=False)
