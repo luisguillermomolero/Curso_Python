c2c265189bcd452cf73128da1c0d90679f6edb07
luisguillermomolero <luisguillermomolero@gmail.com>
2025-06-24 21:01:35 -0500

    commit

--- 07_Modulo_7/02_SEMANA_02/app/routes/user_routes.py
+++ 07_Modulo_7/02_SEMANA_02/app/routes/user_routes.py
@@ -12,3 +12,26 @@ class LoginData(BaseModel):
     username: str
     password: str
 
+@router.post("/login")
+def login(data: LoginData,db: Session = Depends(get_db())):
+    user = db.query(User).filter(User.username == data.username).first()
+    if not user or not verificar_password(data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Credencial inválida")
+    
+    token = crear_token({"sub": user.username})
+    return {"access_token": token, "token_type": "bearer"}
+
+@router.post("/register")
+def register(data: LoginData,db: Session = Depends(get_db())):
+    user = db.query(User).filter(User.username == data.username).first()
+    if user:
+        raise HTTPException(status_code=400, detail="Usuario ya existe")
+    
+    hashed = hashear_password(data.password)
+    nuevo_usuario = User(username=data.username, hashear_password=hashed)
+    db.add(nuevo_usuario)
+    db.commit()
+    db.refresh(nuevo_usuario)
+    return {"mensaje": "Usuario registrado exitosamente"}
+  
+    
