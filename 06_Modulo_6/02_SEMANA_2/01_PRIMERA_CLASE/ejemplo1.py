822b877b663f8dff503cb49813bacc5c8845d91e
Masterdevsenior <lgmoleros@gmail.com>
2025-07-02 19:58:48 -0500

    comm

--- 06_Modulo_6/02_SEMANA_2/01_PRIMERA_CLASE/ejemplo1.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Usuario(BaseModel):
-    nombre: str
-    edad: int
-
-#usuario = Usuario("Luis", "25")
-usuario = Usuario(nombre="Luis", edad="25")
-print(usuario)
--- 06_Modulo_6/03_SEMANA_3/app/init_db.py
+++
@@ -1,9 +0,0 @@
-from database.base import Base, engine
-from models.tarea import Tarea  # Asegúrate de tener este modelo
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
-    print("Base de datos inicializada correctamente")
-
-if __name__ == "__main__":
-    init_db()
