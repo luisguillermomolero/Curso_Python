7793bb48ad11c3cc853105b65524ac3a3a1f0968
Masterdevsenior <lgmoleros@gmail.com>
2025-08-05 19:25:06 -0500

    commit

--- 07_Modulo_7/03_SEMANA_03/01_PRIMERA_CLASE/my_project/app/routers/admin.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Depends
-from app.auth.roles import require_role
-
-router = APIRouter()
-
-@router.get("/admin-data")
-def get_admin_data(current_user=Depends(require_role("admin"))):
-    return {"msg": "Datos solo para administradores"}
