02f335253102ddf25e03a3aab175dac70033372d
Masterdevsenior <lgmoleros@gmail.com>
2025-08-12 21:24:10 -0500

    commit

---
+++ 07_Modulo_7/02_SEMANA_02/app/middleware/validate_token.py
@@ -0,0 +1,15 @@
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.requests import Request
+from fastapi.responses import JSONResponse
+
+class ValidateTokenMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        if request.url.path.startswith("/admin"):
+            token = request.headers.get("Authorization")
+            if token is None:
+                return JSONResponse(
+                    status_code=401,
+                    content={"detail": "Token requerido"}
+                )
+        return await call_next(request)
+
