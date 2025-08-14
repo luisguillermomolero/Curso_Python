cfd5124753fd72d122de284fba30d1cb071f7179
Masterdevsenior <lgmoleros@gmail.com>
2025-08-14 08:00:20 -0500

    commit

--- 07_Modulo_7/01_SEMANA_01/02_Segunda_Clase/02_Ejercicio_02/app/models/user.py
+++ 07_Modulo_7/01_SEMANA_01/02_Segunda_Clase/02_Ejercicio_02/app/models/user.py
@@ -7,6 +7,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
-    create_at = Column(DateTime(timezone=True), server_default=func.now())
-    update_at = Column(DateTime(timezone=True), onupdate=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     
